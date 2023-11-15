46900d863910f18457ecbd7e0effc691d7f4ca86
dcigic92 <dinocigic@yahoo.com>
2023-11-15 15:43:40 +0000

    Push to github

--- run.py
+++ run.py
@@ -49,5 +49,5 @@ def careers():
 if __name__ == "__main__":
     app.run(
         host=os.environ.get("IP", "0.0.0.0"),
-        port=int(os.environ.get("PORT", "2000")),
+        port=int(os.environ.get("PORT", "5000")),
         debug=True)
